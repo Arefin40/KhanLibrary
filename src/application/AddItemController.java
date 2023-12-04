40ea91333e7393a0903c45db44d38bc6e0922f18
ShahriarArefin <arfin17310@gmail.com>
2023-12-04 15:41:15 +0600

    Implemented AddItem

--- src/application/AddItemController.java
+++ src/application/AddItemController.java
@@ -1,9 +1,11 @@
 package application;
 
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.RadioButton;
+import javafx.event.ActionEvent;
 import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class AddItemController {
 
@@ -17,21 +19,36 @@ public class AddItemController {
    private TextField pubtitle, pubcat, pubauthor, pubname, pubyear;
 
    @FXML
-   private RadioButton isjournalno, isjournalyes;
+   private ToggleGroup isJournal;
 
    @FXML
    void bookAdd(ActionEvent event) {
-
+      App.library.addItem(
+            booktitle.getText(),
+            bookcat.getText(),
+            new ArrayList<>(Arrays.asList(bookauthor.getText().split(", "))),
+            Integer.parseInt(bookpbyear.getText()),
+            bookpbname.getText());
    }
 
    @FXML
    void movieAdd(ActionEvent event) {
+      App.library.addItem(
+            movieTitle.getText(),
+            movieCat.getText(),
+            new ArrayList<>(Arrays.asList(moviedirector.getText().split(", "))),
+            Integer.parseInt(movieRelease.getText()));
 
    }
 
    @FXML
    void pubAdd(ActionEvent event) {
-
+      App.library.addItem(
+            pubtitle.getText(),
+            pubcat.getText(),
+            new ArrayList<>(Arrays.asList(pubauthor.getText().split(", "))),
+            Integer.parseInt(pubyear.getText()),
+            isJournal.getSelectedToggle().toString().contains("YES"),
+            pubname.getText());
    }
-
 }
